66b39a4add93e2aef2c8571d1e4a2dabb21c7b57
Hallison Paz <hallison.dapaz@gmail.com>
2021-05-04 11:35:46 -0300

    after edge detection

--- src/filtros.py
+++ src/filtros.py
@@ -1,5 +1,6 @@
 # Apresentação de filtros
 from PIL import Image, ImageFilter
+from math import sqrt
 import os
 from utils import show_vertical, show_horizontal, in_file, out_file
 
@@ -19,8 +20,55 @@ def show_box_blur(filename, r=1):
         )
     )
 
+# IMPC07 | "DETECTANDO ARESTAS na Imagem com Filtro Sobel": https://youtu.be/uHT4qDzq1bY
+def show_edges(filename, direction='x',offset=0):
+    '''Aplica um filtro Sobel à imagem, exibe e salva o resultado'''
+
+    original = Image.open(in_file(filename)).convert('L')
+    XSOBEL = ImageFilter.Kernel((3, 3),
+                                [-1, 0, 1,
+                                -2, 0, 2,
+                                -1, 0, 1],
+                                1, 
+                                offset)
+    YSOBEL = ImageFilter.Kernel((3, 3),
+                                [-1, -2, -1,
+                                0, 0, 0,
+                                1, 2, 1],
+                                1,
+                                offset)
+    if direction == 'x':
+        filtered = original.filter(XSOBEL)
+    elif direction == 'y':
+        filtered = original.filter(YSOBEL)
+    else:
+        vsobel = original.filter(XSOBEL)
+        hsobel = original.filter(YSOBEL)
+        w, h = original.size
+        filtered = Image.new('L', (w, h))
+
+        for i in range(w):
+            for j in range(h):
+                value = sqrt(
+                    vsobel.getpixel((i, j))**2 + hsobel.getpixel((i, j))**2
+                )
+                value = int(min(value, 255))
+                filtered.putpixel((i, j), value)
+
+    # Mostrar as imagens lado a lado
+    show_horizontal(original, filtered)
+    filtered.save(
+        out_file(
+            '{}_{}sobel_{}.jpg'.format(
+                                    filename[:filename.index('.')], 
+                                    direction,
+                                    offset)
+        )
+    )
+
 
 if __name__ == "__main__":
     # Experimente outras imagens e tamanhos de filtros
-    show_box_blur('ellie.jpg', 4)
+    # show_box_blur('ellie.jpg', 4)
+    show_edges('sam.jpg', 'a', 0)
 
